6c2a3a41ab8be1b0f4d260240aa463e3ee393f74
soulen3 <steven@soulen.net>
2025-09-16 10:39:40 -0500

    Fix issue preventing client form starting

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import {
   provideClientHydration,
@@ -15,7 +15,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     ThemeService,
   ],
 };
